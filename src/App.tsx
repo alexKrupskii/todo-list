444439a1e22de2293fb8bdb438d5388e6c639306
alexKrupskii <alex.krupskii91@gmail.com>
2020-05-10 01:58:41 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,21 @@ import {Todolist} from './Todolist';
 
 function App() {
 
-    const tasks1 = [
+    let tasks = [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false }
     ]
-    const tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am Happy", isDone: false },
-        { id: 3, title: "Yo", isDone: false }
-    ]
+
+    const removeTask = (id: number) => {
+        tasks = tasks.filter(t => t.id !== id)
+        }
 
     return (
         <div className="App">
-            <Todolist title="What to learn" tasks={tasks1} />
-            <Todolist title="Songs" tasks={tasks2} />
+            <Todolist title="What to learn" tasks={tasks} removeTask={removeTask} />
         </div>
-    );
+    )
 }
 
 export default App;
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -9,6 +9,7 @@ type TaskType = {
 type PropsType = {
     title: string
     tasks: Array<TaskType>
+    removeTask: Function
 }
 
 export function Todolist(props: PropsType) {
@@ -19,9 +20,10 @@ export function Todolist(props: PropsType) {
             <button>+</button>
         </div>
         <ul>
-            <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-            <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-            <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+            {
+                props.tasks.map( t =>  <li><input type="checkbox" checked={t.isDone}/> <span>{t.title}</span>
+                <button onClick={ () => {props.removeTask(t.id)} }>x</button></li>)
+            }
         </ul>
         <div>
             <button>All</button>
