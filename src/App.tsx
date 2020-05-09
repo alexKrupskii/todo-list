c7ce7ea7bb260124bdc4e5a0d3ea2c8010f57702
alexKrupskii <alex.krupskii91@gmail.com>
2020-05-10 01:06:26 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import styles from './App.module.css';
 import React from 'react';
-import Todolist from "./components/Todolist/Todolist";
+import './App.css';
+import {Todolist} from './Todolist';
 
-export type TaskType = {
-    id: number
-    title: string
-    isDone: boolean
-}
+function App() {
 
-const App = () => {
-    const tasks: Array<TaskType> = [
-        {id: 1, title: "CSS", isDone: true},
-        {id: 2, title: "JS", isDone: false},
-        {id: 3, title: "ReactJS", isDone: false},
-        {id: 4, title: "Patterns", isDone: true}
-    ];
+    const tasks1 = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ]
+    const tasks2 = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I am Happy", isDone: false },
+        { id: 3, title: "Yo", isDone: false }
+    ]
 
     return (
-        <div className={styles.App}>
-            <Todolist tasks={tasks}/>
-            <Todolist tasks={tasks}/>
-            <Todolist tasks={tasks}/>
+        <div className="App">
+            <Todolist title="What to learn" tasks={tasks1} />
+            <Todolist title="Songs" tasks={tasks2} />
         </div>
     );
 }
